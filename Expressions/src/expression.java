404cb0b186560777f56a57521cad4b1ecfc62e11
Glen Bowler <79621348+GlenBowler@users.noreply.github.com>
2022-03-09 10:17:23 +0200

    Add files via upload

---
+++ Expressions/src/expression.java
@@ -0,0 +1,13 @@
+
+public class expression {
+
+	public static void main(String[] args) {
+		// Reminder to when using / to change varaible type to double
+		int amountFriends=10;
+		//Add friends
+		amountFriends=amountFriends+1;// amountFriends++;  
+		amountFriends+=20; //Ading 20 to total friends
+		System.out.println(amountFriends);
+	}
+
+}
